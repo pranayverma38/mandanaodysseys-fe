8dd7ff399ac13bcae73fe477f78fdc235c5ece33
pranayverma38 <pranayverma38@gmail.com>
2026-07-01 05:16:23 +0530

    Update TourTypeLayout to include a container for improved styling: Wrap children in a div with flex and padding classes for better layout presentation on tour type pages.

--- src/app/(app)/destinations/[destination-name]/[type-of-tour]/layout.tsx
+++ src/app/(app)/destinations/[destination-name]/[type-of-tour]/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function TourTypeLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+  return <div className="container flex flex-col gap-y-8 pb-8 pt-10 lg:gap-y-10 lg:pb-12 lg:pt-12">{children}</div>
 }
